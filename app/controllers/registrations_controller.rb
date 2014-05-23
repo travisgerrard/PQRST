8706e4ef757c6169ef8e8cd2a63ece9cb4cb9bf7
Travis Gerrard <travisgerrard@gmail.com>
2014-05-23 08:26:44 -0400

    registration api working

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,33 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :update_sanitized_params, if: :devise_controller?
+  respond_to :json
+
+  def create
+  	logger.info "Registered a Request..."
+  	respond_to do |format|
+  		format.html { super }
+  		format.json {
+  			logger.info "Register Json Request..."
+  			user = User.new(new_user_params)
+    		if user.save
+      			render :json=> { :success=>true, :auth_token=>user.authentication_token, :email=>user.email, :status=>201 }
+      			return
+    		else
+      			warden.custom_failure!
+      			render :json=> user.errors, :status=>422
+    		end
+  		}
+  	end
+  end
 
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:email, :password, :password_confirmation)}
     devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :current_password)}
   end
 
+  private
+
+  	def new_user_params
+  		params.require(:user).permit(:email, :password)
+  	end
 end
